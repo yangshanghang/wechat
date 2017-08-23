7f54243cad9d6732d6656f77b7e7d072af6f76dd
shuyuqiong <shuyuqiong1986@163.com>
2017-08-23 09:41:51 +0800

    配置说明

--- scripts/sql/ddl-20170612.sql
+++
@@ -1,4 +0,0 @@
-
-
-UPDATE `WECHAT_TEMPLATE` SET WT_CONTENT ='first:content:#000000;keyword1:bindNumber:#aaaaaa;keyword2:bindExplain:#aaaaaa' WHERE id ='15266e12edde11e6a00a00188b839ae8';
-INSERT INTO `WECHAT_TEMPLATE` VALUES ('15266e12edde11e6a00a00188b839ae4', 'NTjrwZdy6u7m8O97FKHuLffp2kRby9hwe20DJoyIhQ8', 'yr_notice', 'first:content:#000000;keyword1:yrStatus:#aaaaaa;keyword2:pusher:#aaaaaa;keyword3:pushTime:#aaaaaa;remark:remark:#ff0000');
